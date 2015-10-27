50f6fd51a48470d6b00f07213865acbc23500038
Martin Samuelsson <commit@martsam.se>
2015-10-27 11:07:44 +0100

    Fix false positives when comparing RIV-TA namespaces with TAK namespaces

--- anslutningsplattform/src/groovy/se/skltp/ap/util/TjanstekontraktUtil.groovy
+++ anslutningsplattform/src/groovy/se/skltp/ap/util/TjanstekontraktUtil.groovy
@@ -1,19 +1,25 @@
 package se.skltp.ap.util
 
-import se.skltp.ap.services.dto.TjansteKontraktDTO;
+import java.util.regex.Pattern;
 
 class TjanstekontraktUtil {
+
+	//this is at least a very common pattern
+	static Pattern takNamnrymdPattern = ~'^urn:riv:(.*)Responder:(\\d)$'
 	
 	// match RIV-TA info against TAK-info where (example):
 	// RIV-TA: namnrymd: crm:scheduling:GetSubjectOfCareSchedule
 	// RIV-TA: majorVersion: 1
 	// TAK: namnrymd: urn:riv:crm:scheduling:GetSubjectOfCareScheduleResponder:1
 	//   where the last number is the major version
-	static boolean isNamnrymdEqual(String rivTaNamnrymd, String rivTaMajorVersion,
-			String takNamnrymd) {
-			
-		takNamnrymd.contains(rivTaNamnrymd) &&
-			extractMajorVersionFromTakNamnrymd(takNamnrymd) == rivTaMajorVersion
+	static boolean isNamnrymdEqual(String rivTaNamnrymd, String rivTaMajorVersion, String takNamnrymd) {
+		def matcher = takNamnrymdPattern.matcher(takNamnrymd)
+		if (matcher.matches()) {
+			rivTaNamnrymd == matcher.group(1) && rivTaMajorVersion == matcher.group(2)
+		} else { //resort to simplified comparison
+			takNamnrymd.contains(rivTaNamnrymd) &&
+					extractMajorVersionFromTakNamnrymd(takNamnrymd) == rivTaMajorVersion
+		}
 	}
 		
 	// namnrymd: urn:riv:crm:scheduling:GetSubjectOfCareScheduleResponder:1
