494754ec64b9a0d9cd09744c06a9ec8a37db215b
Martin Samuelsson <commit@martsam.se>
2015-11-08 20:38:40 +0100

    Add sql migration script for Nat change

---
+++ resources/mysql/anslutningsplattform_migration_2015-11-09.sql
@@ -0,0 +1,13 @@
+USE `anslutningsplattform`;
+
+DROP TABLE IF EXISTS `tjanstekomponent_nat`;
+
+ALTER TABLE `tjanstekomponent`
+ADD COLUMN `nat_id` VARCHAR(255) NULL DEFAULT NULL AFTER `ipadress`,
+ADD INDEX `FK_68hng8po96p0anxb73h5yor3y` (`nat_id` ASC);
+ALTER TABLE tjanstekomponent`
+ADD CONSTRAINT `FK_68hng8po96p0anxb73h5yor3y`
+  FOREIGN KEY (`nat_id`)
+  REFERENCES `nat` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
