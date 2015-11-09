e61b185007e1d8a78cd7f3083045592d302d55fe
Martin Samuelsson <commit@martsam.se>
2015-11-09 11:23:23 +0100

    Fix missing ` in migration script

--- resources/mysql/anslutningsplattform_migration_2015-11-09.sql
+++ resources/mysql/anslutningsplattform_migration_2015-11-09.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `tjanstekomponent_nat`;
 ALTER TABLE `tjanstekomponent`
 ADD COLUMN `nat_id` VARCHAR(255) NULL DEFAULT NULL AFTER `ipadress`,
 ADD INDEX `FK_68hng8po96p0anxb73h5yor3y` (`nat_id` ASC);
-ALTER TABLE tjanstekomponent`
+ALTER TABLE `tjanstekomponent`
 ADD CONSTRAINT `FK_68hng8po96p0anxb73h5yor3y`
   FOREIGN KEY (`nat_id`)
   REFERENCES `nat` (`id`)
