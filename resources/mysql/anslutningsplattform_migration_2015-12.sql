6e644396ad3515704e6b3fb3bdf4ce399f87cc99
Martin Samuelsson <commit@martsam.se>
2016-01-06 19:40:19 +0100

    Update db migration script for recent changes

--- resources/mysql/anslutningsplattform_migration_2015-12.sql
+++
@@ -1,6 +0,0 @@
-USE `anslutningsplattform`;
-
-ALTER TABLE `tjanstekomponent`
-CHANGE COLUMN `ipadress` `producent_ipadress` VARCHAR(255) NULL DEFAULT NULL,
-ADD COLUMN `producent_dns_namn` VARCHAR(255) NULL DEFAULT NULL AFTER `producent_ipadress`,
-ADD COLUMN `konsument_ipadress` VARCHAR(255) NULL DEFAULT NULL AFTER `producent_dns_namn`;
