42de7abbe2974819b013b79734a0f95ad7ad481d
Martin Samuelsson <commit@martsam.se>
2016-01-11 07:27:25 +0100

    Remove other_info columns for personkontakt from migration script

--- resources/mysql/anslutningsplattform_migration_2016-01.sql
+++ resources/mysql/anslutningsplattform_migration_2016-01.sql
@@ -3,12 +3,7 @@ USE `anslutningsplattform`;
 ALTER TABLE `tjanstekomponent`
 CHANGE COLUMN `ipadress` `producent_ipadress` VARCHAR(255) NULL DEFAULT NULL,
 ADD COLUMN `producent_dns_namn` VARCHAR(255) NULL DEFAULT NULL AFTER `producent_ipadress`,
-ADD COLUMN `konsument_ipadress` VARCHAR(255) NULL DEFAULT NULL AFTER `producent_dns_namn`,
-ADD COLUMN `huvudansvarig_kontakt_other_info` varchar(255) NULL DEFAULT NULL AFTER `huvudansvarig_kontakt_namn`,
-ADD COLUMN `teknisk_kontakt_other_info` varchar(255) NULL DEFAULT NULL AFTER `teknisk_kontakt_namn`;
-
-ALTER TABLE `personkontakt`
-ADD COLUMN `other_info` VARCHAR(255) NULL DEFAULT NULL AFTER `namn`;
+ADD COLUMN `konsument_ipadress` VARCHAR(255) NULL DEFAULT NULL AFTER `producent_dns_namn`;
 
 ALTER TABLE `konsumentbestallning`
 ADD COLUMN `namn_pa_etjanst` VARCHAR(255) NULL DEFAULT NULL AFTER `version`;
